f3f48cfcff23b3f1fffdaf881ee658ca1064874d
Luma <luma@luma.co.za>
2025-12-08 14:55:30 +0200

    merged chris, updated version, after houdini xpu updates

--- client/ayon_deadline/version.py
+++ client/ayon_deadline/version.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 """Package declaring AYON addon 'deadline' version."""
-__version__ = "0.5.19-ls.0.1.2"
+__version__ = "0.5.19-ls.0.1.3"
--- package.py
+++ package.py
@@ -1,6 +1,6 @@
 name = "deadline"
 title = "Deadline"
-version = "0.5.19-ls.0.1.2"
+version = "0.5.19-ls.0.1.3"
 
 client_dir = "ayon_deadline"
 
