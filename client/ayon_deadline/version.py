02e1cb9ddf818787da257efb060ad273d2fe702d
llienofdoom <neill.vermaak@gmail.com>
2026-03-17 10:50:48 +0200

    build: bump version to 0.7.1+ls.0.2.0

--- client/ayon_deadline/version.py
+++ client/ayon_deadline/version.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 """Package declaring AYON addon 'deadline' version."""
-__version__ = "0.7.0+ls.0.1.5"
+__version__ = "0.7.1+ls.0.2.0"
--- package.py
+++ package.py
@@ -1,6 +1,6 @@
 name = "deadline"
 title = "Deadline"
-version = "0.7.0+ls.0.1.5"
+version = "0.7.1+ls.0.2.0"
 
 client_dir = "ayon_deadline"
 
